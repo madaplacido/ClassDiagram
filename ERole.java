fe355f0210d7f0ae5d19c5c82d9c4ddfedc27013
Madalena <m.placido@campus.fct.unl.pt>
2023-10-12 22:18:07 +0100

    Aula pratica 4

---
+++ ERole.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class ERole {
+
+    /**
+     * Default constructor
+     */
+    public ERole() {
+    }
+
+    /**
+     * Tomei a liberdade de dizer que e' um atributo privado String a conselho do professor das
+     * pra'ticas, visto que o enunciado na'o especifica nada e da' erro de outra maneira. O diagrama
+     * no entanto continua igual ao disponibilizado (ou seja, sem tipo e privado por default)
+     */
+    private String lecture;
+
+    /**
+     * Tomei a liberdade de dizer que e' um atributo privado String a conselho do professor das
+     * pra'ticas, visto que o enunciado na'o especifica nada e da' erro de outra maneira. O diagrama
+     * no entanto continua igual ao disponibilizado (ou seja, sem tipo e privado por default)
+     */
+    private String tutor;
+
+    /**
+     * Tomei a liberdade de dizer que e' um atributo privado String a conselho do professor das
+     * pra'ticas, visto que o enunciado na'o especifica nada e da' erro de outra maneira. O diagrama
+     * no entanto continua igual ao disponibilizado (ou seja, sem tipo e privado por default)
+     */
+    private String examiner;
+
+}
---
+++ ESemester.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class ESemester {
+
+    /**
+     * Default constructor
+     */
+    public ESemester() {
+    }
+
+    /**
+     * Tomei a liberdade de dizer que e' um atributo privado String a conselho do professor das
+     * pra'ticas, visto que o enunciado na'o especifica nada e da' erro de outra maneira. O diagrama
+     * no entanto continua igual ao disponibilizado (ou seja, sem tipo e privado por default)
+     */
+    private String winter;
+
+    /**
+     * Tomei a liberdade de dizer que e' um atributo privado String a conselho do professor das
+     * pra'ticas, visto que o enunciado na'o especifica nada e da' erro de outra maneira. O diagrama
+     * no entanto continua igual ao disponibilizado (ou seja, sem tipo e privado por default)
+     */
+    private String summer;
+
+}
